0a9adfc1fc41763063b6698f759a14f1dc8a0763
guygreenleaf <guygreenleaf@icloud.com>
2021-03-07 23:25:09 -0800

    Initial Commit

---
+++ MasterMindRework/MasterMindReworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterMindReworkApp.swift
+//  MasterMindRework
+//
+//  Created by Guy Greenleaf on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MasterMindReworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
